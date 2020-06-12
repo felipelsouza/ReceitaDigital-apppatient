4780e4ff2d51df87c42f25508a2f2c27201fdefa
Felipe <felipelsouza@unipam.edu.br>
2020-06-11 21:30:33 -0300

    layout tela de receitas

--- src/screens/Recipes.js
+++ src/screens/Recipes.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 
+import Swiper from 'react-native-swiper'
+
 import commonStyles from '../commonStyles'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
@@ -9,13 +11,76 @@ import 'moment/locale/pt-br'
 
 import api from '../services/api'
 
+const initialState = {
+    medicaments: null,
+    dosages: null,
+    obs: null,
+}
+
 export default class Recipes extends Component {
     state = {
-
+        ...initialState,
+        recipes: []
     }
 
-    render() {
+    async componentDidMount() {
+        await api.get('/receitas/10765536641')
+            .then(res => this.setState({ recipes: res.data }))
+        const arr = this.state.recipes
+
+        var meds = arr.map((function (meds) {
+            return meds.MEDICAMENTO_RECEITA
+        }))
+
+        var dos = arr.map((function (dos) {
+            return dos.DOSAGEM
+        }))
+
+        var obs = arr.map((function (obs) {
+            return obs.OBS_RECEITA_PACIENTE
+        }))
+
+        this.setState({ medicaments: meds, dosages: dos, obs: obs })
+    }
+
+    renderRecipes() {
+        const { recipes, medicaments, dosages, obs } = this.state
         const today = moment().locale('pt-br').format('DD/MM/YYYY')
+
+        if (recipes.length === 0) {
+            return (
+                <View style={styles.container}>
+                    <View style={styles.header}>
+                        <TouchableOpacity style={styles.addIcon} activeOpacity={0.8}
+                            onPress={() => this.props.navigation.navigate('Home')}>
+                            <Icon name="angle-left" size={15}
+                                color={commonStyles.colors.secondary}
+                            />
+                        </TouchableOpacity>
+                        <Text style={styles.title}>Receita Digital</Text>
+                        <Text style={styles.title}>{today}</Text>
+                    </View>
+                    <View
+                        style={
+                            styles.body,
+                            {
+                                flexDirection: 'column',
+                                paddingHorizontal: '10%',
+                                paddingVertical: '50%'
+                            }}
+                    >
+                        <View style={{ alignItems: 'center' }}>
+                            <Icon name="folder-open" size={120}
+                                color={'#00FF00'}
+                                style={{ opacity: 0.3 }}
+                            />
+                            <Text style={styles.emptyData}>Ops..</Text>
+                            <Text style={styles.emptyData}>Você não tem receitas disponíveis!</Text>
+                        </View>
+                    </View>
+                </View>
+            )
+        }
 
         return (
             <View style={styles.container}>
@@ -29,28 +94,32 @@ export default class Recipes extends Component {
                     <Text style={styles.title}>Receita Digital</Text>
                     <Text style={styles.title}>{today}</Text>
                 </View>
-                <View>
-                    <Text>NOME</Text>
-                    <Text>CPF</Text>
+                <View style={styles.body}>
+                    <View>
+                        <ScrollView>
+                            <Text>{medicaments}</Text>
+                            <Text>{dosages}</Text>
+                            <Text>{obs}</Text>
+                        </ScrollView>
+                    </View>
                 </View>
-                <View>
-                    <ScrollView>
-                        <Text>Medicamento</Text>
-                        <Text>Dosagem</Text>
-                        <Text>Obs</Text>
-                    </ScrollView>
-                </View>
-                <View>
-                    <Text>Medico</Text>
-                    <Text>CRM</Text>
-                    <Text>UF</Text>
-                </View>
-            </View>
+            </View >
+        )
+
+    }
+
+    render() {
+
+        return (
+            <Swiper style={styles.wrapper} showsButtons={true}>
+                {this.renderRecipes()}
+            </Swiper>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    wrapper: {},
     container: {
         flex: 1
     },
@@ -61,12 +130,20 @@ const styles = StyleSheet.create({
         borderColor: commonStyles.colors.primaryDark,
         justifyContent: 'space-between'
     },
+    body: {
+        //flex: 10,
+        padding: '10%'
+    },
     title: {
         fontWeight: 'bold',
         color: commonStyles.colors.primary,
         fontSize: 15
     },
-
+    emptyData: {
+        fontSize: 18,
+        color: commonStyles.colors.primaryDark,
+        fontWeight: 'bold'
+    },
     addIcon: {
         flexDirection: 'row',
         width: 24,
@@ -76,5 +153,4 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-
 })
